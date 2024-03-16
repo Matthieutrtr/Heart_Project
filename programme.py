014484ad3879dd12718d423685fccac9ba1f1be8
imeneBouafia <imn167.tb@gmail.com>
2024-03-16 14:03:41 +0100

    correction de la log_densite mais tjrs pas de bons resultats

--- programme.py
+++ programme.py
@@ -6,41 +6,51 @@ import numpy as np
 import scipy.stats as sp 
 import matplotlib.pyplot as plt 
 
-def heartbeats(sd, sigma, tau, nchain, after_drugs, before_drugs):
-    PVC_count = after_drugs + before_drugs
+
+
+def heartbeats(sd_alpha, sd_delta, sd_hyperparam, nchain, hearts_data):
     #2 parameters to estimate 
     chain = np.zeros((nchain+1, 2)) #alpha and delta 
     #init at 0,0 (told so in the data folder) --> p = 0.5 and theta = 0.5
     #rate of acceptance 
     acceptance = 0
     
+    #data
+    t = hearts_data[:, 0]
+    x = hearts_data[:, 1]
+    y = hearts_data[:, 2]
+    
     #density 
-    def log_g(alpha, delta, y, t):
+    def log_g(alpha, delta, t, x, y,sigma, tau):
         #vector that indicates the value where the is no PVC's
-        y_null = (y == 0) * 1
-        n0 = y_null.sum()
-        n1 = (1-y_null).sum()
+        idx0 = where(y ==0)
+        idx1 = where(y !=0)
+        y0 = y[idx0] #vecteur sans PVC's
+        y1 = y[idx1]
+        x1 = y[idx1]
+        t0 = t[idx0]
+        t1 = t[idx1]
+        n0 = y_0.sum() 
+        n1 = (y_1).sum()
         
         sum1 = -((alpha/sigma)**2 + (delta/tau)**2)/2
-        
-        sum2 = n1 * np.log(2 *np.exp(delta)/ (1+np.exp(delta))) + alpha * y.sum() - \
-            np.log(1+np.exp(alpha)) * (t * (1-y_null)).sum()
-            
-        sum3 = (np.log(np.exp(delta) + (1+np.exp(alpha))**(-t)) * y_null).sum() - \
-            -np.log(1+np.exp(delta)) * n0
+        sum2 = n1 * np.log(1+np.exp(delta)) - np.log(1+np.exp(-alpha)) * y1.sum() - np.log(1+np.exp(alpha)) * x1.sum()
+        sum3 = (np.log( (1+np.exp(-delta))**(-1) + (1+np.exp(alpha))**(t0) * (1+np.exp(delta))**(-1))).sum()
+
         return sum1 +sum2+sum3
     
     for i in range(nchain):
-        new_candidate = chain[i, :] + np.random.normal(size = 2, scale= sd)
-        
+        index = np.random.choice([0,1])
+        chain[i+1, :] = chain[i,:]
+        chain[i+1, index] = chain[i, index] + np.random.normal(scale = np.sqrt(sd_hyperparam[index])) #changement d'une seule coord
+        #print(new_candidate)
         #proposal kernel symetric --> only the difference between the log-densities 
-        ratio = np.exp(log_g(new_candidate[0], new_candidate[1], after_drugs,PVC_count) - \
-            log_g(chain[i,0], chain[i, 1], after_drugs, PVC_count))
-        #print(ratio)
+        ratio = np.exp(log_g(chain[i+1, 0], chain[i+1, 1], t, x, y, sd_hyperparam[0], sd_hyperparam[1]) - \
+            log_g(chain[i,0], chain[i, 1], t, x, y, sd_hyperparam[0], sd_hyperparam[1]))
+        print(ratio)
         #MAJ 
         u = np.random.uniform()
         if u < ratio: 
-            chain[i+1, : ] = new_candidate
             acceptance += 1
         else:
             chain[i+1, :] = chain[i, :]
@@ -72,10 +82,10 @@ x = np.array([6, 9, 17, 22, 7, 5, 5, 14, 9, 7, 9, 51])
 #after drugs 
 y = np.array([5, 2, 0, 0, 2, 1, 0, 0, 0, 0, 13, 0])
 
-sigma = 1e-2
-tau = 1e-2
+sigma = 10000
+tau = 10000
 
-chain, accep = heartbeats(.03, sigma, tau, 11000, y, t)
+chain, accep = heartbeats(.1, 10, sigma, tau, 11000, y, t)
 print(accep)
 plt.plot(chain[1000:, 0])
 plt.show()
@@ -89,9 +99,9 @@ delta = chain[:, 1]
 
 fig, ax = plt.subplot_mosaic([["A", "A"],
                               ["B", "B"]])
-ax["A"].hist(alpha[1000:], bins= 'auto', label= r"Posteriori $\alpha$");
+ax["A"].hist(alpha[1000:], bins= 'auto', label= r"Posteriori $\alpha$", density = True);
 ax["A"].legend()
-ax["B"].hist(delta[1000:], bins= 'auto', label= r"Posteriori $\delta$");
+ax["B"].hist(delta[1000:], bins= 'auto', label= r"Posteriori $\delta$", density = True);
 ax["B"].legend()
 plt.show()
 
